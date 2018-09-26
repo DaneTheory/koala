0fed2d81b4b6bb6880d9593ed8904db38bfa6b35
Dom Charlesworth <dgc336@gmail.com>
2018-09-26 07:35:23 +0100

    Get the watch command printing out babel command

--- bin/koala.js
+++ bin/koala.js
@@ -1,9 +1,40 @@
 #!/usr/bin/env node
-const { resolve } = require('path')
-const { _: args } = require('minimist')(process.argv.slice(2))
+const { resolve, dirname, basename } = require('path')
+const args = require('minimist')(process.argv.slice(2))
+const { createMonitor } = require('watch')
 const shortid = require('shortid')
 
-const [ path = '' ] = args
-const fullpath = resolve(process.cwd(), path)
+const [ path = '' ] = args._
 
-console.log(fullpath)
+const errorAndQuit = (...msg) => {
+  console.error(...msg)
+  process.exit(0)
+}
+
+const loadConfig = (conf) => {
+  const cwd = process.cwd()
+  const configPath = resolve(cwd, conf)
+  try {
+    return require(configPath)
+  } catch (e) {
+    errorAndQuit('Cannot find config file', configPath)
+  }
+}
+
+const cwd = process.cwd()
+// const config = loadConfig(args['config'] || args['c'])
+const watchDir = resolve(cwd, path)
+const outDir = resolve(cwd, args['out-dir'] || args['o'])
+
+createMonitor(watchDir, { interval: 0.2 }, monitor => {
+  monitor.on('changed', f => {
+    const file = basename(f, '.js')
+    const outFile = f
+      .replace(watchDir, outDir)
+      .replace(file, shortid())
+
+    const command = `npx babel ${f} --out-file ${outFile} --plugins='babel-plugin-koala'`
+
+    console.log(command)
+  })
+})
